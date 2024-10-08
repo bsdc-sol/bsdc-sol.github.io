022ae92bbefbefa5481fee05bc012b5ea57d372f
bsdc-sol <30268146@bsdc.ac.uk>
2024-10-08 16:23:31 +0100

    added actions to top menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,37 @@
-const facebook = document.getElementById("#facebook")
-const hobbyButton = document.querySelector("#hobbies")
-const achieveButton = document.querySelector("#achieve")
+const services = document.getElementById("servicesButton");
+const about = document.getElementById("aboutButton");
+const help = document.getElementById("helpButton");
+const facebook = document.getElementById("facebook");
+const instagram = document.getElementById("instagram");
+const linkedin = document.getElementById("linkedin");
+const whatsapp = document.getElementById("whatsapp");
+const twitter = document.getElementById("twitter");
 
+services.addEventListener("click", function () {
+  document.getElementById("services").scrollIntoView();
+});
+about.addEventListener("click", function () {
 
+});
+help.addEventListener("click", function () {
 
+});
 facebook.addEventListener("click", function () {
-  console.log("facebook");
-  alert("Button");
+  window.location.href = "http://www.facebook.com";
 });
-hobbyButton.addEventListener("click", function () {
-  window.location.href = "hobbies.html";
+instagram.addEventListener("click", function () {
+  window.location.href = "http://www.instagram.com";
 });
-achieveButton.addEventListener("click", function () {
-  window.location.href = "achieve.html";
+linkedin.addEventListener("click", function () {
+  window.location.href = "http://www.linkedin.com";
+});
+whatsapp.addEventListener("click", function () {
+  alert("Mobile Number: +44 7589 549390");
+});
+twitter.addEventListener("click", function () {
+  window.location.href = "http://www.x.com";
+});
+top.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
